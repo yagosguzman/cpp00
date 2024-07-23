c99d9bc476438550ef65e5145fd8905a88e3de38
ysanchez <yagosguzman@gmail.com>
2024-07-23 21:39:58 +0200

    ex00 done, ex01 WIP

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -3,11 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ysanchez <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ysanchez <ysanchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 20:04:45 by ysanchez          #+#    #+#             */
-/*   Updated: 2024/06/07 20:05:06 by ysanchez         ###   ########.fr       */
+/*   Updated: 2024/07/23 21:07:51 by ysanchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <cctype>
 
+void	upperstr(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+	{
+		std::cout << (char)toupper(str[i]);
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+	if (argc > 1)
+	{
+		while (++i < argc)
+			upperstr(argv[i]);
+		std::cout << std::endl;
+	}
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return (0);
+}
